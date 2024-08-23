b6e79c7df7d6df99e0443e1c12256f236de44f32
David M Fobes <dfobes@lanl.gov>
2024-08-23 09:31:30 -0600

    WIP: ADD: convert enums to refs

--- ravens/xml/export_cim_xml.py
+++ ravens/xml/export_cim_xml.py
@@ -9,10 +9,17 @@ from rdflib.namespace import Namespace
 from rdflib.term import URIRef, Literal
 from rdflib import Graph, RDF
 
+from ravens.cim_tools.common import get_names_of_enumeration_classes
+from ravens.io import parse_eap_data
+
 
 class RavensExport(object):
-    def __init__(self, data):
+    def __init__(self, data, core_data_path=None):
         self.data = deepcopy(data)
+        self.cim_enums = None
+        if core_data_path is not None:
+            core_data = parse_eap_data(core_data_path)
+            self.cim_enums = get_names_of_enumeration_classes(core_data.objects)
 
         self.graph = Graph()
         self.cim = Namespace("http://iec.ch/TC57/CIM100#")
@@ -20,6 +27,8 @@ class RavensExport(object):
 
         self.build_rdf_graph(self.data)
         self.update_uri_refs()
+        if self.cim_enums is not None:
+            self.update_enumeration_uri_refs()
 
     def build_rdf_graph(self, data, obj_name=None):
         for k, v in data.items():
@@ -71,6 +80,7 @@ class RavensExport(object):
         triple_to_delete = []
         for (p, o), (cim_type, obj_name) in po_to_update.items():
             target_subject = None
+            # TODO: how to handle if using IdentifiedObject.name vs IdentifiedObject.mRID?
             try:
                 target_subject = self.graph.value(predicate=self.cim["IdentifiedObject.name"], object=Literal(obj_name), any=False)
             except UniquenessError as msg:
@@ -89,10 +99,21 @@ class RavensExport(object):
         for triple in triple_to_delete:
             self.graph.remove(triple)
 
+    def update_enumeration_uri_refs(self):
+        triple_to_delete = []
+        for s, p, o in self.graph.triples():
+            if isinstance(o, Literal):
+                if any(o.startswith(enum) for enum in self.cim_enums):
+                    self.graph.add((s, p, self.cim[str(o)]))
+                    triple_to_delete.append((s, p, o))
+
+        for triple in triple_to_delete:
+            self.graph.remove(triple)
+
 
 if __name__ == "__main__":
     with open("out/test_xml2json_case3.json", "r") as f:
         d = json.load(f)
 
-    r = RavensExport(d)
+    r = RavensExport(d, core_data_path="cim/iec61970cim17v40_iec61968cim13v13b_iec62325cim03v17b_CIM100.1.1iec61970cim17v40_iec61968cim13v13b_iec62325cim03v17b_CIM100.1.1.1_mgravens24v1.eap")
     r.graph.serialize("out/test_output.xml", max_depth=1, format="pretty-xml")
