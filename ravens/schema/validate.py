34e6ad30ca7273f1b0053982a8a59bf70dec7e88
David M Fobes <dfobes@lanl.gov>
2025-03-03 12:52:10 -0700

    ADD: ability to use in-memory schema in validator

--- ravens/schema/validate.py
+++ ravens/schema/validate.py
@@ -4,26 +4,39 @@ import pathlib
 from jschon import create_catalog, JSON, JSONSchema, URI, LocalSource, RemoteSource
 
 from ravens.data import _RAVENS_SCHEMA_BASE_URL
+from ravens.schema import RavensSchema
 from ravens.logging import logger
 
 class RavensValidator:
-    def __init__(self, schema_base_uri: str = _RAVENS_SCHEMA_BASE_URL, schema_url: str | None = None, local_path_to_schema: pathlib.PosixPath | None = None) -> None:
+    def __init__(self, schema_base_uri: str = _RAVENS_SCHEMA_BASE_URL, schema_url: str | None = None, local_path_to_schema: pathlib.PosixPath | None = None, schema: RavensSchema | None = None) -> None:
         self.catalog = create_catalog("2020-12")
 
-        if schema_url is None:
-            schema_url = "/".join([schema_base_uri, "Root.json"])
+        if schema is not None:
+            self.schema = JSONSchema(schema.schema)
 
-        if local_path_to_schema is not None:
-            self.catalog.add_uri_source(URI(schema_base_uri), LocalSource(local_path_to_schema.parent, suffix=""))
-            self.schema = JSONSchema.loadf(local_path_to_schema)
         else:
-            self.catalog.add_uri_source(None, RemoteSource(URI(schema_base_uri)))
-            self.schema = JSONSchema.loadr(URI(schema_url))
+            if schema_url is None:
+                schema_url = "/".join([schema_base_uri, "Root.json"])
 
+            if local_path_to_schema is not None:
+                self.catalog.add_uri_source(URI(schema_base_uri), LocalSource(local_path_to_schema.parent, suffix=""))
+                self.schema = JSONSchema.loadf(local_path_to_schema)
+            else:
+                self.catalog.add_uri_source(None, RemoteSource(URI(schema_base_uri)))
+                self.schema = JSONSchema.loadr(URI(schema_url))
+
+        self.data = None
+        self.result = None
+
+    def _clear_data(self):
+        self.data = None
         self.result = None
 
     def validate(self, data: JSON, print_result: bool = True):
-        self.result = self.schema.evaluate(data)
+        self._clear_data()
+
+        self.data = data
+        self.result = self.schema.evaluate(self.data)
 
         if print_result:
             self.print_result()
@@ -56,6 +69,9 @@ if __name__ == "__main__":
     # Local version of Validator
     schema_path = pathlib.Path(os.getcwd()) / "out/schema/separate/Root.json"
     validator = RavensValidator(schema_base_uri=f"file://{os.getcwd()}/out/schema/separate/", local_path_to_schema=schema_path)
+
+    # geneate schema from scratch
+    validator = RavensValidator(schema = RavensSchema())
 
     # Validation example
     data_dir = pathlib.Path(os.getcwd()) / "examples/schema"
