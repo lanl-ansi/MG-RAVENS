dbe57c5550f2843ad0a8a6007b83dcce91baf2d2
David M Fobes <dfobes@lanl.gov>
2025-03-06 08:46:02 -0700

    UPD: improve cyme conversion

--- ravens/xml/cyme.py
+++ ravens/xml/cyme.py
@@ -29,19 +29,34 @@ class CymeConverter:
         self.graph.bind("cim", self.cim_ns, override=True)
         self.graph.bind("cyme", self.cyme_ns, override=True)
 
+        self.to_remove = set()
+
         # TODO: other load models
         self.load_response_values = {"CYMELoadFormat.kw_kvar": {"name": "Constant kVA", "values": {"pConstantPower": 100.0, "qConstantPower": 100.0}}}
         self.load_response_uris = {}
 
+        self.equip_container_uris = {}
+
         self.convert_cyme_cable_concentric_neutrals()
         self.convert_cyme_customer_class()
+        self.convert_cyme_customer_load()
         self.convert_cyme_customer_load_value()
+        self.convert_cyme_structure()
+        self.convert_cyme_connection_status()
+        self.convert_cimconductingequipment_structure_id()
 
         self.fix_EnergyConsumerPhase()
         self.fix_WireInfo()
+        self.fix_Terminal()
 
         if prune_remaining_cyme:
             self.remove_cyme_objects()
+
+        self.prune_triples()
+
+    def prune_triples(self):
+        for triple in self.to_remove:
+            self.graph.remove(triple)
 
     def build_cim_obj(self, rdf_type: str, mrid: str | None = None, name: str | None = None, skip_mrid: bool = False) -> URIRef:
         if mrid is None:
@@ -65,39 +80,33 @@ class CymeConverter:
             self.graph.add((subject, self.cim_ns[predicate], Literal(str(obj))))
 
     def remove_cyme_objects(self):
-        to_remove = set()
         for s, o in self.graph.subject_objects(predicate=RDF.type):
-            if o.startswith(str(self.cyme_ns)):
+            if str(o).startswith(str(self.cyme_ns)):
                 for triple in self.graph.triples((s, None, None)):
-                    to_remove.add(triple)
+                    self.to_remove.add(triple)
 
         for p in self.graph.predicates():
-            if p.startswith(str(self.cyme_ns)):
+            if str(p).startswith(str(self.cyme_ns)):
                 for triple in self.graph.triples((None, p, None)):
-                    to_remove.add(triple)
-
-        for triple in to_remove:
-            self.graph.remove(triple)
+                    self.to_remove.add(triple)
 
     def convert_cyme_cable_concentric_neutrals(self):
-        cableinfo_triples_to_remove = set()
         cableinfo_to_replace = set()
-        cccn_to_prune = set()
         for s in self.graph.subjects(predicate=RDF.type, object=self.cim_ns["CableInfo"]):
             cccn_ref = self.graph.value(subject=s, predicate=self.cyme_ns["CYMECableConstruction.CableConcentricNeutrals"])
             if cccn_ref is not None:
-                node = self.build_cim_obj("ConcentricNeutralCableInfo", name=self.graph.value(subject=s, predicate=self.cim_ns["IdentifiedObject.name"]))
+                node = self.build_cim_obj("ConcentricNeutralCableInfo", name=str(self.graph.value(subject=s, predicate=self.cim_ns["IdentifiedObject.name"])))
 
                 for p, o in self.graph.predicate_objects(subject=s):
-                    cableinfo_triples_to_remove.add((s, p, o))
-                    if not p.startswith(str(self.cyme_ns)) and p != RDF.type:
-                        self.add_triple(node, p.split("#", maxsplit=2)[-1], o)
+                    self.to_remove.add((s, p, o))
+                    if not str(p).startswith(str(self.cyme_ns)) and p != RDF.type:
+                        self.add_triple(node, str(p).split("#", maxsplit=2)[-1], o)
 
-                neutral_radius = literal_eval(self.graph.value(subject=cccn_ref, predicate=self.cyme_ns["CYMECableConcentricNeutrals.wireDiameter"]).value) / 2
+                neutral_radius = literal_eval(self.graph.value(subject=cccn_ref, predicate=self.cyme_ns["CYMECableConcentricNeutrals.wireDiameter"]).value) / 2  # type: ignore
                 neutral_gmr = neutral_radius * 0.7788
-                rdc20 = material_resistivity.get(self.graph.value(subject=cccn_ref, predicate=self.cyme_ns["CYMECableConcentricNeutrals.material"]).split("#")[-1], 2e-8) / (math.pi * neutral_radius**2)
-                neutral_count = literal_eval(self.graph.value(subject=cccn_ref, predicate=self.cyme_ns["CYMECableConcentricNeutrals.numberOfWires"]).value)
-                diameter_over_neutral = literal_eval(self.graph.value(subject=cccn_ref, predicate=self.cyme_ns["CYMECableConcentricNeutrals.outerDiameter"]).value)
+                rdc20 = material_resistivity.get(str(self.graph.value(subject=cccn_ref, predicate=self.cyme_ns["CYMECableConcentricNeutrals.material"])).split("#")[-1], 2e-8) / (math.pi * neutral_radius**2)
+                neutral_count = literal_eval(self.graph.value(subject=cccn_ref, predicate=self.cyme_ns["CYMECableConcentricNeutrals.numberOfWires"]).value)  # type: ignore
+                diameter_over_neutral = literal_eval(self.graph.value(subject=cccn_ref, predicate=self.cyme_ns["CYMECableConcentricNeutrals.outerDiameter"]).value)  # type: ignore
 
                 self.add_triple(node, "ConcentricNeutralCableInfo.neutralStrandRadius", neutral_radius)
                 self.add_triple(node, "ConcentricNeutralCableInfo.neutralStrandGmr", neutral_gmr)
@@ -106,44 +115,31 @@ class CymeConverter:
                 self.add_triple(node, "ConcentricNeutralCableInfo.diameterOverNeutral", diameter_over_neutral)
 
                 cableinfo_to_replace.add((s, node))
-                cccn_to_prune.add(cccn_ref)
+                self.to_remove.add((cccn_ref, None, None))
             else:
                 for p, o in self.graph.predicate_objects(subject=s):
-                    if p.startswith(str(self.cyme_ns)):
-                        cableinfo_triples_to_remove.add((s, p, o))
+                    if str(p).startswith(str(self.cyme_ns)):
+                        self.to_remove.add((s, p, o))
 
         for old_o, new_o in cableinfo_to_replace:
             for s, p, _ in list(self.graph.triples((None, None, old_o))):
                 self.graph.add((s, p, new_o))
-                self.graph.remove((s, p, old_o))
-
-        for triple in cableinfo_triples_to_remove:
-            self.graph.remove(triple)
-
-        for cccn in cccn_to_prune:
-            for triple in self.graph.triples((cccn, None, None)):
-                self.graph.remove(triple)
-
-    def convert_cyme_cable_construction(self):
-        # nothing to do
-        pass
+                self.to_remove.add((s, p, old_o))
 
     def convert_cyme_customer_class(self):
-        to_prune = set()
         for s in self.graph.subjects(predicate=RDF.type, object=self.cyme_ns["CYMECustomerClass"]):
-            to_prune.add(s)
+            self.to_remove.add((s, None, None))
             node = self.build_cim_obj("LoadGroup", name=str(self.graph.value(subject=s, predicate=self.cim_ns["IdentifiedObject.name"])))
             for _s in self.graph.subjects(predicate=self.cyme_ns["CYMECustomerLoad.CustomerClass"], object=s):
                 assert self.graph.value(subject=_s, predicate=RDF.type) == self.cyme_ns["CYMECustomerLoad"]
                 ec_uri = self.graph.value(subject=_s, predicate=self.cyme_ns["CYMECustomerLoad.EnergyConsumer"])
-                self.add_triple(ec_uri, "EnergyConsumer.LoadGroup", node)
+                self.add_triple(URIRef(str(ec_uri)), "EnergyConsumer.LoadGroup", node)
 
     def convert_cyme_customer_load(self):
-        # nothing to do
-        pass
+        for s in self.graph.subjects(predicate=RDF.type, object=self.cyme_ns["CYMECustomerLoad"]):
+            self.to_remove.add((s, None, None))
 
     def convert_cyme_customer_load_value(self):
-        sub_to_prune = set()
         for s in self.graph.subjects(predicate=RDF.type, object=self.cyme_ns["CYMECustomerLoadValue"]):
             cust_load = self.graph.value(subject=s, predicate=self.cyme_ns["CYMECustomerLoadValue.CustomerLoad"])
             ec_uri = self.graph.value(subject=cust_load, predicate=self.cyme_ns["CYMECustomerLoad.EnergyConsumer"])
@@ -151,19 +147,11 @@ class CymeConverter:
             for k in ["p", "q"]:
                 v = self.graph.value(subject=s, predicate=self.cyme_ns[f"CYMECustomerLoadValue.{k}"])
                 if v is not None:
-                    self.add_triple(ec_uri, f"EnergyConsumer.{k}", v)
+                    self.add_triple(URIRef(str(ec_uri)), f"EnergyConsumer.{k}", v)
 
-            self.add_LoadResponseCharacteristic(ec_uri, self.graph.value(subject=s, predicate=self.cyme_ns["CYMECustomerLoadValue.loadFormat"]).split("#")[-1])
+            self.add_LoadResponseCharacteristic(URIRef(str(ec_uri)), str(self.graph.value(subject=s, predicate=self.cyme_ns["CYMECustomerLoadValue.loadFormat"])).split("#")[-1])
 
-            sub_to_prune.add(s)
-
-        to_remove = set()
-        for s in sub_to_prune:
-            for triple in self.graph.triples((s, None, None)):
-                to_remove.add(triple)
-
-        for triple in to_remove:
-            self.graph.remove(triple)
+            self.to_remove.add((s, None, None))
 
     def add_LoadResponseCharacteristic(self, subject_uri: URIRef, cyme_load_format: str):
         lrc_values = self.load_response_values.get(cyme_load_format, None)
@@ -177,17 +165,35 @@ class CymeConverter:
 
             self.add_triple(subject_uri, "EnergyConsumer.LoadResponse", self.load_response_uris[cyme_load_format])
 
+    def convert_cyme_structure(self):
+        for s in self.graph.subjects(predicate=RDF.type, object=self.cyme_ns["CYMEStructure"]):
+            self.to_remove.add((s, None, None))
+
+    def convert_cyme_connection_status(self):
+        for s, p, o in self.graph.triples((None, self.cyme_ns["CYMEConnectionStatus.connectionStatusType"], None)):
+            if str(o).endswith("Connected"):
+                self.add_triple(URIRef(str(s)), "Equipment.inService", True)
+            else:
+                self.add_triple(URIRef(str(s)), "Equipment.inService", False)
+
+            self.to_remove.add((s, p, o))
+
+    def convert_cimconductingequipment_structure_id(self):
+        for s, p, o in self.graph.triples((None, self.cyme_ns["CIMConductingEquipment.StructureID"], None)):
+            if o not in self.equip_container_uris:
+                node = self.build_cim_obj("EquipmentContainer", name=str(o))
+                self.equip_container_uris[o] = node
+
+            self.add_triple(URIRef((str(s))), "Equipment.EquipmentContainer", self.equip_container_uris[o])
+            self.to_remove.add((s, p, o))
+
     def fix_EnergyConsumerPhase(self):
-        triple_to_prune = set()
         for s in self.graph.subjects(predicate=RDF.type, object=self.cim_ns["EnergyConsumerPhase"]):
             for k in ["p", "q"]:
                 v = self.graph.value(subject=s, predicate=self.cim_ns[f"EnergyConsumerPhase.{k}Fixed"])
                 if v is not None:
-                    self.add_triple(s, f"EnergyConsumerPhase.{k}", v)
-                    triple_to_prune.add((s, self.cim_ns[f"EnergyConsumerPhase.{k}Fixed"], v))
-
-        for triple in triple_to_prune:
-            self.graph.remove(triple)
+                    self.add_triple(URIRef((str(s))), f"EnergyConsumerPhase.{k}", v)
+                    self.to_remove.add((s, self.cim_ns[f"EnergyConsumerPhase.{k}Fixed"], v))
 
     def fix_WireInfo(self):
         for wi in ["WireInfo", "CableInfo", "OverheadWireInfo", "ConcentricNeutralCableInfo", "TapeShieldCableInfo"]:
@@ -197,15 +203,18 @@ class CymeConverter:
 
                 if self.graph.value(subject=s, predicate=self.cim_ns["WireInfo.gmr"]) is None:
                     if radius is not None:
-                        self.add_triple(s, "WireInfo.gmr", literal_eval(radius.value) * 0.7788)
+                        self.add_triple(URIRef(str(s)), "WireInfo.gmr", literal_eval(radius.value) * 0.7788)  # type: ignore
 
                 if self.graph.value(subject=s, predicate=self.cim_ns["WireInfo.rDC20"]) is None:
                     if radius is not None and material is not None:
-                        self.add_triple(s, "WireInfo.rDC20", material_resistivity.get(str(material).split("#")[-1], 2e-8) / (math.pi * literal_eval(radius.value) ** 2))
+                        self.add_triple(URIRef(str(s)), "WireInfo.rDC20", material_resistivity.get(str(material).split("#")[-1], 2e-8) / (math.pi * literal_eval(radius.value) ** 2))  # type: ignore
 
-    def convert_cyme_structure(self):
-        # nothing to do
-        pass
+    def fix_Terminal(self):
+        for s in self.graph.subjects(predicate=RDF.type, object=self.cim_ns["Terminal"]):
+            seq = self.graph.value(subject=s, predicate=self.cim_ns["Terminal.sequenceNumber"])
+            if seq is not None:
+                self.add_triple(URIRef(str(s)), "ACDCTerminal.sequenceNumber", seq)
+                self.to_remove.add((s, self.cim_ns["Terminal.sequenceNumber"], seq))
 
     def save(self, path: pathlib.PosixPath | str):
         self.graph.serialize(path, max_depth=1, format="pretty-xml", base="")
