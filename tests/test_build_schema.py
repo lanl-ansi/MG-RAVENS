fb3b5b8cf8b874205a427c3a90e94a43f499c3bc
David M Fobes <dfobes@lanl.gov>
2025-03-10 14:07:56 -0600

    UPD: schema unit tests

--- tests/test_build_schema.py
+++ tests/test_build_schema.py
@@ -3,9 +3,10 @@ import pytest
 from ravens.schema import RavensSchema
 from jschon import JSONSchema
 
+
 def test_build_schema():
     schema = RavensSchema()
     assert schema
-    assert len(schema.schemas) == 922
+    assert len(schema.schemas) == 933
 
     assert JSONSchema(schema.schema)
