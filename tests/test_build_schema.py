12f80f8aba072689f8fda0099e1c0c56bd3c19c5
David M Fobes <dfobes@lanl.gov>
2025-03-12 19:57:16 -0600

    FIX: Example Schemas

--- tests/test_build_schema.py
+++ tests/test_build_schema.py
@@ -7,6 +7,6 @@ from jschon import JSONSchema
 def test_build_schema():
     schema = RavensSchema()
     assert schema
-    assert len(schema.schemas) == 933
+    assert len(schema.schemas) == 937
 
     assert JSONSchema(schema.schema)
