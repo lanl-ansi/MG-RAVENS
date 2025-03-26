297bc527ea2bade8ba7a6c024f9ba922b4f7eaeb
David M Fobes <dfobes@lanl.gov>
2025-03-26 13:47:05 -0600

    FIX: Curve

--- tests/test_build_schema.py
+++ tests/test_build_schema.py
@@ -7,6 +7,6 @@ from jschon import JSONSchema
 def test_build_schema():
     schema = RavensSchema()
     assert schema
-    assert len(schema.schemas) == 937
+    assert len(schema.schemas) == 944
 
     assert JSONSchema(schema.schema)
