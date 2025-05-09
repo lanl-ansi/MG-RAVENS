d5a5fcbd2bbacbe22a7f42c1cb427994db48cec4
jjospina <jjospina@lanl.gov>
2025-05-09 12:07:23 -0600

    ADD: WIP PowerModelsProtection information to PowerElectronicsConnection.

--- tests/test_build_schema.py
+++ tests/test_build_schema.py
@@ -7,6 +7,6 @@ from jschon import JSONSchema
 def test_build_schema():
     schema = RavensSchema()
     assert schema
-    assert len(schema.schemas) == 945
+    assert len(schema.schemas) == 951
 
     assert JSONSchema(schema.schema)
