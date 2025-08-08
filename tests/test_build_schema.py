4baa0f60e69566321c458842169e8e176b9b2da0
David M Fobes <dfobes@lanl.gov>
2025-08-08 07:50:25 -0600

    FIX: template and base data

--- tests/test_build_schema.py
+++ tests/test_build_schema.py
@@ -7,6 +7,6 @@ from jschon import JSONSchema
 def test_build_schema():
     schema = RavensSchema()
     assert schema
-    assert len(schema.schemas) == 950
+    assert len(schema.schemas) == 954
 
     assert JSONSchema(schema.schema)
