23d4817809d82a34bc1552cc1d148988bf16ef22
David M Fobes <dfobes@lanl.gov>
2025-09-04 08:46:56 -0600

    ADD: ProducerCostFunctions

--- tests/test_build_schema.py
+++ tests/test_build_schema.py
@@ -7,6 +7,6 @@ from jschon import JSONSchema
 def test_build_schema():
     schema = RavensSchema()
     assert schema
-    assert len(schema.schemas) == 995
+    assert len(schema.schemas) == 1001
 
     assert JSONSchema(schema.schema)
